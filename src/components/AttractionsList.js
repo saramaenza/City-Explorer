f206634702e6388254edcd3945dd0221d9ec5127
SaraMaenza <sara.maenza98@gmail.com>
2025-12-30 14:20:15 +0100

    Add component AttractionsList

---
+++ src/components/AttractionsList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import LoadingSpinner from "./LoadingSpinner";
+
+function AttractionsList({ attractions, loading }) {
+  return (
+    <>
+      <h2 className="font-bebas text-4xl">Main attraction</h2>
+      {loading && (
+        <div className="flex items-center mt-8">
+          <LoadingSpinner text="attractions" />
+        </div>
+      )}
+      {!loading && (
+        <>
+          {attractions.length > 0 ? (
+            <ul className="list-disc font-work text-lg pl-8 mt-8 grid grid-cols-2 gap-x-8 gap-y-2">
+              {attractions.map((attr, idx) => (
+                <li key={idx} className="relative">
+                  <a
+                    href={attr.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-900 relative group"
+                  >
+                    {attr.name}
+                    <span className="absolute left-0 right-0 -bottom-1 h-1 w-0 bg-yellow-400 rounded transition-all duration-300 group-hover:w-full"></span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="font-work text-lg mt-8">No attractions found.</div>
+          )}
+        </>
+      )}
+    </>
+  );
+}
+
+export default AttractionsList;
