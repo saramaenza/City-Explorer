b6b3d039d7db4657ebe55710dc107fec0b9b881a
SaraMaenza <sara.maenza98@gmail.com>
2025-12-30 14:21:45 +0100

    Add component HeroHomepage

---
+++ src/components/HeroHomepage.js
@@ -0,0 +1,27 @@
+import HeroSection from "./HeroSection";
+import SearchInput from './SearchInput';
+import PhotoCredit from "./PhotoCredit";
+
+const UNSPLASH_IMAGE_URL = 'https://images.unsplash.com/photo-1519451241324-20b4ea2c4220?...';
+
+function HeroHomepage() {
+  return (
+    <>
+      <HeroSection
+        image={UNSPLASH_IMAGE_URL}
+        title="Explore the cities in the world"
+        subtitle="Find your next destination"
+        homepage={true}
+      >
+        <SearchInput />
+      </HeroSection>
+      
+      <PhotoCredit 
+        author="Robin Noguier" 
+        url="https://unsplash.com/@robinnoguier?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" 
+        downloadUrl="https://unsplash.com/photos/sydwCr54rf0/download?force=true"/>
+    </>
+  );
+}
+
+export default HeroHomepage;
