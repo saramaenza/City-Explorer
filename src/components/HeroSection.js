35068cdecf7a30f57b82c6428e2508887554fe18
SaraMaenza <sara.maenza98@gmail.com>
2025-12-30 17:45:52 +0100

    Update title style

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,12 +4,12 @@ function HeroSection({ image, title, subtitle, homepage, children }) {
   return (
     <BackgroundHero image={image}>
       <h1
-        className={`font-bebas text-6xl text-white drop-shadow-lg text-center tracking-wide ${!homepage ? "pt-20" : ""}`}
+        className={`font-bebas text-6xl text-white drop-shadow-lg text-shadow-lg text-center tracking-wide ${!homepage ? "pt-20" : ""}`}
       >
         {title}
       </h1>
       {subtitle && (
-        <h4 className="font-work text-xl font-medium text-white mt-3 drop-shadow-lg text-center">
+        <h4 className="font-work text-xl font-medium text-white mt-3 drop-shadow-lg text-shadow-lg text-center">
           {subtitle}
         </h4>
       )}
