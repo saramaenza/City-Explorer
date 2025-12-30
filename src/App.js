b637ef8bf80592a24ee345412de50b5b2c95fea9
SaraMaenza <sara.maenza98@gmail.com>
2025-12-30 14:23:24 +0100

    Update component App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import './App.css';
 import CitySuggestions from './components/CitySuggestions';
-import Hero from './components/Hero';
+import HeroHomepage from './components/HeroHomepage';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import CityDetail from './components/CityDetail';
+import Explore from './components/Explore';
+import ScrollToTop from "./components/ScrollToTop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const suggestedCities = [
+  { name: "Rome", country: "Italy", wikiCode: "Q220" },
+  { name: "Paris", country: "France", wikiCode: "Q90" },
+  { name: "New York", country: "USA", wikiCode: "Q60" },
+  { name: "London", country: "United Kingdom", wikiCode: "Q84" },
+];
+
+const text = "Let us inspire you";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <CitySuggestions />
-      <Footer />
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroHomepage />
+                <div className="py-10">
+                  <CitySuggestions suggestedCities={suggestedCities} text={text} />
+                </div>
+              </>
+            }
+          />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/city/:cityName/:wikiCode" element={<CityDetail />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
